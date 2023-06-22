c11f043f722d2b6a30b2f7bb0bf653f9ce2c4854
dorgee <dorgeelama91@gmail.com>
2023-06-22 10:13:27 -0600

    fixed bugs related to index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ import { FilterProvider } from './context/filter_context';
 import { CartProvider } from './context/cart_context';
 import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <ProductsProvider>
     <App />
-  </ProductsProvider>,
-  document.getElementById('root')
+  </ProductsProvider>
 );
