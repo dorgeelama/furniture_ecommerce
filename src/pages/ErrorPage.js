b181d6b4b9747af7a132140a5547077ae6287b53
dorgee <dorgeelama91@gmail.com>
2023-06-27 16:33:25 -0600

    added link to go home to error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -7,6 +7,9 @@ const ErrorPage = () => {
       <section>
         <h1>404</h1>
         <h3>Sorry, the page you tried cannot be found</h3>
+        <Link to='/' className='btn'>
+          back home
+        </Link>
       </section>
     </Wrapper>
   );
